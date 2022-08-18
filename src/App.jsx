19388c57da4bee6ecf8d9bc6036dddd821abc8a8
Aaron Rodi <metalhead5209@gmail.com>
2022-08-18 12:07:26 -0500

    restructured code with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-
-import NavBar from './components/NavBar/NavBar';
-import './App.css';
-import Hero from './components/Hero/Hero';
-
-
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 function App() {
-  
-
   return (
     <div className="App">
       <NavBar />
       <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
