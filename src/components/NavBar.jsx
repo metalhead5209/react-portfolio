ddc20e44774bc4a96cf73eddbb116358b85ea5ae
Aaron Rodi <metalhead5209@gmail.com>
2022-08-17 19:44:11 -0500

    finished navbar styling, make functional later

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,52 +2,95 @@ import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Logo from "../assets/logo.png";
-
+import GitIcon from "../assets/github.svg";
+import LinkIcon from "../assets/linkedin.svg";
+import TwitIcon from "../assets/twitter.svg";
 
 const NavBar = () => {
-    const [ activeLink, setActiveLink ] = useState('home')
-    const [ srolled, setScrolled ] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            }else {
-                setScrolled(false)
-            }
-        }
-        window.addEventListener('scroll', onScroll)
-    }, [])
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
 
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const updateActiveLink = (val) => {
+    setActiveLink(val);
+  };
 
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img 
-           src={Logo} 
-           alt="logo" 
-           className="logo-name"
-           />
+          <img src={Logo} alt="logo" className="logo-name" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => updateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => updateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => updateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                {/* <a href="#"> <img src={} alt={} /></a>
-                <a href="#"> <img src={} alt={} /></a>
-                <a href="#"> <img src={} alt={} /></a> */}
+              <a href="#">
+                <img src={GitIcon} alt='github' 
+                className='soc-icon'
+                />
+              </a>
+              <a href="#">
+                <img src={LinkIcon} alt='linkedin' 
+                className='soc-icon'
+                />
+              </a>
+              <a href="#">
+                <img src={TwitIcon} alt='twitter' 
+                className='soc-icon'
+                />
+              </a>
             </div>
-            <button className='contact-btn' onclick={() => console.log('Connect')}><span>Contact Me</span></button>
+            <button
+              className="contact-btn"
+              onClick={() => console.log("Connect")}
+            >
+              <span>Contact Me</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
