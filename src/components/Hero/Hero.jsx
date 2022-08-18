7deeb37c2dce591553c23bc708aa171f9c0004ea
Aaron Rodi <metalhead5209@gmail.com>
2022-08-18 09:22:32 -0500

    added styling to hero section. complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+
 import "./Hero.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Me from '../../assets/profile.jpg';
 
 const Hero = () => {
+
+
+
   return (
     <section className="hero" id="home">
       <Container>
@@ -11,7 +14,7 @@ const Hero = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="greeting">Welcome to my Personal Portfolio!</span>
             <h1>
-              {`Hi, My name is Aaron Rodi`}
+              {`Hi, My name is Aaron Rodi,`} <br />
               <span
                 className="
                     wrap"
@@ -23,7 +26,7 @@ const Hero = () => {
               Passionate Software Developer. I’ll be graduating in December with
               my AAS in Web Development. When I’m not doing my school work, I am
               building projects and refining my skills. This is my passion. I am
-              30 years old and spent the majority of my life wondering if I’d
+              31 years old and spent the majority of my life wondering if I’d
               ever have a legitimate career in something I’m passionate about
               and I have finally found it. I would highly appreciate any advice
               from people currently in or pursuing a career in development. One
@@ -32,7 +35,7 @@ const Hero = () => {
             </p>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={Me} alt='profile img' />
+            <img src={Me} className='profile-img' alt='profile img' />
           </Col>
         </Row>
       </Container>
