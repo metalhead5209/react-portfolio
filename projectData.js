7f0a06cd5f90b2ec8853c65d61a11ea1c117a7cd
Aaron Rodi <metalhead5209@gmail.com>
2022-08-20 14:22:36 -0500

    dd

--- projectData.js
+++ projectData.js
@@ -4,7 +4,7 @@ import Festi from './src/assets/festishare.png'
 const PROJECT_DATA = [
   {
     key: 1,
-    imgSrc: "./assets/festishare.png",
+    imgSrc: "./src/assets/festishare.png",
     title: "FestiShare",
     techStack:
       "Node.Js | Express | MongoDB | Mongoose | JavaScript | EJS | CSS | Bootstrap Morgan | Multer | Joi | MapBox | PassPort | DotENV",
@@ -15,7 +15,7 @@ const PROJECT_DATA = [
   },
   {
     key: 2,
-    imgSrc: "./assets/emp_mgmt.png",
+    imgSrc: "./src/assets/emp_mgmt.png",
     title: "Employee Management System",
     techStack:
       "Node.JS | Express | MySQL | JavaScript | CSS MySQL WorkBench | HandleBars | Nodemon",
@@ -25,7 +25,7 @@ const PROJECT_DATA = [
   },
   {
     key: 3,
-    imgSrc: "./assets/book-lib.png",
+    imgSrc: "./src/assets/book-lib.png",
     title: "Book Library",
     techStack:
       "Node.Js | Express | MongoDB | Mongoose | JavaScript | CSS Body-parser | Handlebars | DotENV",
@@ -36,7 +36,7 @@ const PROJECT_DATA = [
 
   {
     key: 5,
-    imgSrc: "./assets/port-proj.png",
+    imgSrc: "./src/assets/port-proj.png",
     title: "React Portfolio",
     techStack: "React | JSX | JavaScript | Bootstrap | CSS",
     demoURL: "#",
@@ -45,7 +45,7 @@ const PROJECT_DATA = [
   },
   {
     key: 4,
-    imgSrc: "./assets/sw-pic-blog.png",
+    imgSrc: "./src/assets/sw-pic-blog.png",
     title: "Star Wars Photo Blog",
     techStack: "HTML | CSS | JAVASCRIPT",
     demoURL: "http://cmin.dcc.edu/users/aaronrodi/final%20project/home.html",
